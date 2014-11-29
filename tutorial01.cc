bff59e254e4a928163c8ea5edc03390684ec0648
Alexander Aprelev <aam@chromium.org>
2014-11-29 09:14:15 -0800

    Added perspective

--- tutorial01.cc
+++ tutorial01.cc
@@ -14,6 +14,7 @@ GLFWwindow* window;
 
 // Include GLM
 #include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp> 
 using namespace glm;
 
 void error_callback(int error, const char* description) {
@@ -145,41 +146,62 @@ int main(void) {
   // Dark blue background
   glClearColor(.0f, .0f, 0.4f, 0.0f);
 
-  // An array of 3 vectors which represents 3 vertices
-  static const GLfloat g_vertex_buffer_data[] = {
-      -1.0f, -1.0f,  0.0f,
-       1.0f, -1.0f,  0.0f,
-       0.0f,  1.0f,  0.0f,
-  };
-
-  // This will identify our vertex buffer
-  GLuint vertexbuffer;
-
-  // Generate 1 buffer, put the resulting identifier in vertexbuffer
-  glGenBuffers(1, &vertexbuffer);
-
-  // The following commands will talk about our 'vertexbuffer' buffer
-  glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
-
-  // Give our vertices to OpenGL.
-  glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data),
-               g_vertex_buffer_data, GL_STATIC_DRAW);
-
-  GLuint vao = 0;
-  glGenVertexArrays (1, &vao);
-  glBindVertexArray (vao);
-  glEnableVertexAttribArray (0);
-  glBindBuffer (GL_ARRAY_BUFFER, vertexbuffer);
-  glVertexAttribPointer (0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
-
-  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+  GLuint VertexArrayID = 0;
+  glGenVertexArrays (1, &VertexArrayID);
+  glBindVertexArray (VertexArrayID);
 
   // Create and compile our GLSL program from the shaders
   GLuint programID = LoadShaders("SimpleVertexShader.vertexshader",
                                  "SimpleFragmentShader.fragmentshader");
+
+  GLuint MatrixID = glGetUniformLocation(programID, "MVP");
+
+  // Projection matrix : 45° Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
+  glm::mat4 Projection = glm::perspective(45.0f, 4.0f / 3.0f, 0.1f, 100.0f);
+  // Camera matrix
+  glm::mat4 View       = glm::lookAt(
+      glm::vec3(4,3,3), // Camera is at (4,3,3), in World Space
+      glm::vec3(0,0,0), // and looks at the origin
+      glm::vec3(0,1,0)  // Head is up (set to 0,-1,0 to look upside-down)
+  );
+  // Model matrix : an identity matrix (model will be at the origin)
+  glm::mat4 Model      = glm::mat4(1.0f);  // Changes for each model !
+  // Our ModelViewProjection : multiplication of our 3 matrices
+  glm::mat4 MVP        = Projection * View * Model; // Remember, matrix multiplication is the other way around
+
+  // An array of 3 vectors which represents 3 vertices
+  static const GLfloat g_vertex_buffer_data[] = {
+      -0.5f, -0.5f,  0.0f,
+       0.5f, -0.5f,  0.0f,
+       0.0f,  0.5f,  0.0f,
+
+      -0.5f, -0.5f,  0.0f,
+       0.5f, -0.5f,  0.0f,
+       0.0f, -0.5f,  0.0f,
+  };
+
+  GLuint vertexbuffer;
+  glGenBuffers(1, &vertexbuffer);
+  glBindBuffer(GL_ARRAY_BUFFER, vertexbuffer);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(g_vertex_buffer_data),
+               g_vertex_buffer_data, GL_STATIC_DRAW);
+
+   
+
   do {
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
     glUseProgram(programID);
-    glBindVertexArray(vao);
+    // Send our transformation to the currently bound shader,
+    // in the "MVP" uniform
+    // For each model you render, since the MVP will be different (at least the M part)
+    glUniformMatrix4fv(MatrixID, 1, GL_FALSE, &MVP[0][0]);
+
+    glEnableVertexAttribArray (0);
+    glBindBuffer (GL_ARRAY_BUFFER, vertexbuffer);
+    glVertexAttribPointer (0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+
+    glBindVertexArray(VertexArrayID);
 
     // Draw the triangle !
     glDrawArrays(GL_TRIANGLES, 0,
@@ -194,6 +216,11 @@ int main(void) {
   }  // Check if the ESC key was pressed or the window was closed
   while (glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS &&
          glfwWindowShouldClose(window) == 0);
+
+  // Cleanup VBO and shader
+  glDeleteBuffers(1, &vertexbuffer);
+  glDeleteProgram(programID);
+  glDeleteVertexArrays(1, &VertexArrayID);
 
   // Close OpenGL window and terminate GLFW
   glfwTerminate();
